5ef10fb6b63e623a8950398b71382d69c38548ff
karan <karan1988@gmail.com>
2020-06-21 07:31:16 +0530

    first commit

---
+++ src/test/java/qaclickacademy/restapiTest.java
@@ -0,0 +1,20 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class restapiTest {
+	
+	@Test
+	public void postjira()
+	{
+		System.out.println("postjira");
+		
+	}
+
+	@Test
+	public void deletetwitter()
+	{
+		System.out.println("deletetwitter");
+		
+	}
+}
